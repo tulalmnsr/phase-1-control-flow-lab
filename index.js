62e9ff5f602f708e1c4f83b9f78351a196c2e533
Batoul El Mansour <elmansourbatoul@gmail.com>
2022-10-17 22:14:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue<=400){
+    return "This one is on me!"
+ }else if(someValue>400 && someValue<2000){
+  return 'That will be twenty bucks.'}else if(someValue>2000 && someValue<2500){
+  return 'I will gladly take your thirty bucks.'}else if (someValue>2500){
+  return 'No can do.'}
 }
-
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  if(city==="NYC"){
+    return "Ok, sounds good."
+  }else{
+    return "No go."
+  }
 }
-
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  if (tip === "generous") {
+    return "Thank you so much.";
+} else if (tip === "not as generous") {
+    return "Thank you.";
+} else {
+    return "Bye.";
 }
